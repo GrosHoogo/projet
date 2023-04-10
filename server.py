b085e817ce810560629b07618d620416fe7586af
TARTAIRO <72879269+TARTAIRO@users.noreply.github.com>
2023-04-10 20:52:49 +0200

    Server

---
+++ server.py
@@ -0,0 +1,48 @@
+import socket
+from _thread import *
+import sys
+
+
+server = "192.168.1.29" # Modifier IP lorsque je me déplace
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(4)
+print("En attente de connexion, Server allumé")
+
+
+def threaded_client(conn):
+
+    conn.send(str.encode("Connecté"))
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Déconnecté")
+                break
+            else:
+                print("Recue: ", reply)
+                print("Envoi en cours : ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Connexion perdue")
+    conn.close()
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connecter à:", addr)
+
+    start_new_thread(threaded_client, (conn,))
